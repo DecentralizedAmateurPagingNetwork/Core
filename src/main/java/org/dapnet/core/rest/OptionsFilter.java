8d176b68dcb429266a224cc08d10be517262a7f0
Philipp Thiel <taronyu@users.noreply.github.com>
2020-05-19 00:21:25 +0200

    Added experimental OPTIONS and CORS filters to the REST server

---
+++ src/main/java/org/dapnet/core/rest/OptionsFilter.java
@@ -0,0 +1,23 @@
+package org.dapnet.core.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@PreMatching
+final class OptionsFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+			requestContext.abortWith(Response.status(Status.NO_CONTENT).build());
+		}
+	}
+
+}
